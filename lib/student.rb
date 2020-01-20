44fd9fabdfe1eec026337fd88266e64c780fa676
Nicholas Wilson <nicholas.wilson@nevada.unr.edu>
2020-01-20 22:59:12 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -6,8 +6,13 @@ class Student
   attr_reader :id
 
   def initialize(name, grade, id = nil)
+    @id = id
     @name = name
     @grade = grade
+  end
+
+  def self.create_table
+    sql = <-SQL
   end
 
 end
