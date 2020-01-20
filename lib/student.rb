eb5385ff58dae7702d9ef354d2f80bf11c3c535b
Nicholas Wilson <nicholas.wilson@nevada.unr.edu>
2020-01-20 23:21:37 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -11,6 +11,15 @@ class Student
     @grade = grade
   end
 
+  def save
+    sql = <<-SQL
+      INSERT INTO students (name, grade)
+      VALUES (?,?)
+    SQL
+    DB[:conn].execute(sql, self.name, self.grade)
+    @id = DB[:conn].execute("SELECT last_insert_rowid() FROM students")[0][0]
+  end
+
   def self.create_table
     sql = <<-SQL
       CREATE TABLE IF NOT EXIST students (
@@ -27,16 +36,6 @@ class Student
       DROP TABLE students
     SQL
     DB[:conn].execute(sql)
-  end
-
-  def save
-    sql = <<-SQL
-      INSERT INTO students (name, grade)
-      VALUES (?,?)
-    SQL
-    DB[:conn].execute(sql, self.name, self.grade)
-
-    @id = DB[:conn].execute("SELECT last_insert_rowid() FROM students")[0][0]
   end
 
   def self.create(name, grade)
