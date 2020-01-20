a2de8494351e232b28efc8fc4de1a4c9105bc872
Nicholas Wilson <nicholas.wilson@nevada.unr.edu>
2020-01-20 22:57:13 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -1,7 +1,7 @@
 class Student
 
   # Remember, you can access your database connection anywhere in this class
-  #  with DB[:conn]  
+  #  with DB[:conn]
   attr_accessor :name, :grade
   attr_reader :id
 
@@ -9,5 +9,5 @@ class Student
     @name = name
     @grade = grade
   end
-  
+
 end
