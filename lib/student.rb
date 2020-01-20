ce74c649cb39b5e76a64e7a75d0652d66f1264ca
Nicholas Wilson <nicholas.wilson@nevada.unr.edu>
2020-01-20 23:11:27 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -17,7 +17,14 @@ class Student
         id INTEGER PRIMARY KEY,
         name Text,
         grade INTEGER
-      );
+      )
+      SQL
+    DB[:conn].execute(sql)
+  end
+
+  def self.drop_table
+    sql = <<-SQL
+      DROP TABLE students
       SQL
     DB[:conn].execute(sql)
   end
